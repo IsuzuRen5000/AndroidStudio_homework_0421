a57ba82a67fa72fdd767c67e6293af06beca1d9c
Joelll <f36862512f@gmail.com>
2025-04-21 15:33:16 +0800

    完成跳頁
    已完成第一頁到第二頁

--- app/src/main/java/com/example/intent11210323/MainActivity.kt
+++ app/src/main/java/com/example/intent11210323/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.example.intent11210323
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intent11210323.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        binding.button2.setOnClickListener {
+//            val my = Intent(this,SecondPage::class.java)
+//            startActivity(my)
+//            Intent(this,SecondPage::class.java).apply {
+//                startActivity(this)
+//            }
+//        }
+        binding.button2.setOnClickListener {
+            Intent(this,SecondPage::class.java).apply {
+                putExtra("title","john")
+                putExtra("age",20)
 
+                startActivity(this)
+            }
+        }
     }
 }
