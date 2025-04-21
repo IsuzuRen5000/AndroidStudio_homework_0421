910b4ac9068ff2e297d3593e1c2d97c862311495
Joelll <f36862512f@gmail.com>
2025-04-21 15:43:39 +0800

    完成資料傳遞,尚未測試

--- app/src/main/java/com/example/intent11210323/SecondPage.kt
+++ app/src/main/java/com/example/intent11210323/SecondPage.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intent11210323.databinding.ActivitySecondPageBinding
 
 class SecondPage : AppCompatActivity() {
+    private lateinit var binding2: ActivitySecondPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second_page)
+        binding2=ActivitySecondPageBinding.inflate(layoutInflater)
+        setContentView(binding2.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val p2Title = intent.getStringExtra("title")
+        val p2age = intent.getIntExtra("age", 0)
+        binding2.p2text.text = "Title : $p2Title\nAge : $p2age"
     }
 }
